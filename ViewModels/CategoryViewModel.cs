83c1e4f5a5338a02b6ac30b4f13eee94d7f15a74
BaoDoanto <baodoanto733@gmail.com>
2026-05-15 19:50:44 +0700

    Cap nhat do an phan mem quan ly khach san

---
+++ ViewModels/CategoryViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using QLKS.Models;
+
+namespace QLKS.ViewModels
+{
+    public class CategoryViewModel : BaseViewModel
+    {
+        private HotelManagementEntities _db = new HotelManagementEntities();
+
+        // Danh sách hiển thị lên UI
+        public ObservableCollection<LoaiPhong> ListLoaiPhong { get; set; }
+        public ObservableCollection<DichVu> ListDichVu { get; set; }
+
+        // Đối tượng ràng buộc khi Chọn dòng trên DataGrid hoặc điền Form
+        private LoaiPhong _selectedLoaiPhong;
+        public LoaiPhong SelectedLoaiPhong
+        {
+            get => _selectedLoaiPhong;
+            set { _selectedLoaiPhong = value; OnPropertyChanged(); }
+        }
+
+        private DichVu _selectedDichVu;
+        public DichVu SelectedDichVu
+        {
+            get => _selectedDichVu;
+            set { _selectedDichVu = value; OnPropertyChanged(); }
+        }
+
+        // Commands cho Loại Phòng
+        public ICommand AddLoaiPhongCmd { get; set; }
+        public ICommand EditLoaiPhongCmd { get; set; }
+        public ICommand DeleteLoaiPhongCmd { get; set; }
+
+        // Commands cho Dịch Vụ
+        public ICommand AddDichVuCmd { get; set; }
+        public ICommand EditDichVuCmd { get; set; }
+        public ICommand DeleteDichVuCmd { get; set; }
+
+        public CategoryViewModel()
+        {
+            LoadData();
+
+            // Khởi tạo các Command CRUD Loại Phòng
+            AddLoaiPhongCmd = new RelayCommand<object>(p => true, p => {
+                var lp = new LoaiPhong { TenLoaiPhong = "Loại phòng mới", GiaPhong = 0, SoGiuong = 2, NgayTao = System.DateTime.Now };
+                _db.LoaiPhongs.Add(lp);
+                _db.SaveChanges();
+                ListLoaiPhong.Add(lp);
+            });
+
+            EditLoaiPhongCmd = new RelayCommand<object>(p => SelectedLoaiPhong != null, p => {
+                _db.SaveChanges();
+                MessageBox.Show("Cập nhật loại phòng thành công!");
+            });
+
+            DeleteLoaiPhongCmd = new RelayCommand<object>(p => SelectedLoaiPhong != null, p => {
+                if (MessageBox.Show("Bạn có chắc chắn muốn xóa?", "Xác nhận", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    _db.LoaiPhongs.Remove(SelectedLoaiPhong);
+                    _db.SaveChanges();
+                    ListLoaiPhong.Remove(SelectedLoaiPhong);
+                }
+            });
+
+            // Khởi tạo các Command CRUD Dịch Vụ
+            AddDichVuCmd = new RelayCommand<object>(p => true, p => {
+                var dv = new DichVu { TenDichVu = "Dịch vụ mới", GiaDichVu = 0, NgayTao = System.DateTime.Now };
+                _db.DichVus.Add(dv);
+                _db.SaveChanges();
+                ListDichVu.Add(dv);
+            });
+
+            EditDichVuCmd = new RelayCommand<object>(p => SelectedDichVu != null, p => {
+                _db.SaveChanges();
+                MessageBox.Show("Cập nhật dịch vụ thành công!");
+            });
+
+            DeleteDichVuCmd = new RelayCommand<object>(p => SelectedDichVu != null, p => {
+                if (MessageBox.Show("Bạn có chắc muốn xóa dịch vụ này?", "Xác nhận", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    _db.DichVus.Remove(SelectedDichVu);
+                    _db.SaveChanges();
+                    ListDichVu.Remove(SelectedDichVu);
+                }
+            });
+        }
+
+        private void LoadData()
+        {
+            ListLoaiPhong = new ObservableCollection<LoaiPhong>(_db.LoaiPhongs.ToList());
+            ListDichVu = new ObservableCollection<DichVu>(_db.DichVus.ToList());
+        }
+    }
+}
---
+++ ViewModels/RoomViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using QLKS.Models;
+
+namespace QLKS.ViewModels
+{
+    public class RoomViewModel : BaseViewModel
+    {
+        private HotelManagementEntities _db = new HotelManagementEntities();
+
+        public ObservableCollection<Phong> ListPhong { get; set; }
+        public ObservableCollection<LoaiPhong> ListLoaiPhongCombo { get; set; } // Dùng cho ComboBox chọn loại phòng
+        public ObservableCollection<string> ListTrangThai { get; set; } // Trống, Đang thuê, Đang dọn, Bảo trì
+
+        private Phong _selectedPhong;
+        public Phong SelectedPhong
+        {
+            get => _selectedPhong;
+            set { _selectedPhong = value; OnPropertyChanged(); }
+        }
+
+        public ICommand AddPhongCmd { get; set; }
+        public ICommand EditPhongCmd { get; set; }
+        public ICommand DeletePhongCmd { get; set; }
+
+        public RoomViewModel()
+        {
+            ListPhong = new ObservableCollection<Phong>(_db.Phongs.ToList());
+            ListLoaiPhongCombo = new ObservableCollection<LoaiPhong>(_db.LoaiPhongs.ToList());
+            ListTrangThai = new ObservableCollection<string> { "Trống", "Đang thuê", "Đang dọn", "Bảo trì" };
+
+            AddPhongCmd = new RelayCommand<object>(p => true, p => {
+                if (!ListLoaiPhongCombo.Any()) return;
+
+                var pNew = new Phong
+                {
+                    SoPhong = "Pxxx",
+                    MaLoaiPhong = ListLoaiPhongCombo.First().MaLoaiPhong,
+                    TrangThai = "Trống",
+                    NgayTao = System.DateTime.Now
+                };
+                _db.Phongs.Add(pNew);
+                _db.SaveChanges();
+                ListPhong.Add(pNew);
+            });
+
+            EditPhongCmd = new RelayCommand<object>(p => SelectedPhong != null, p => {
+                _db.SaveChanges();
+                MessageBox.Show("Cập nhật thông tin phòng thành công!");
+            });
+
+            DeletePhongCmd = new RelayCommand<object>(p => SelectedPhong != null, p => {
+                if (MessageBox.Show("Xóa phòng này?", "Cảnh báo", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    _db.Phongs.Remove(SelectedPhong);
+                    _db.SaveChanges();
+                    ListPhong.Remove(SelectedPhong);
+                }
+            });
+        }
+    }
+}
