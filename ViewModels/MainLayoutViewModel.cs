2494c6d858b8aabb19afbab4f2c5c394f7fc6db4
An-whitehat <annguyen062006@gmail.com>
2026-06-03 10:30:09 +0700

    feat: add InvoiceImport (chon dich vu) to sidebar and router

--- ViewModels/MainLayoutViewModel.cs
+++ ViewModels/MainLayoutViewModel.cs
@@ -70,7 +70,10 @@ namespace QLKS.ViewModels
                     NavigateTo(page, "Quản lý Khách hàng", new CustomerViewModel());
                     break;
                 case "Invoice":
-                    NavigateTo(page, "Hóa đơn", new InvoiceCheckoutViewModel());
+                    NavigateTo(page, "Hóa đơn thanh toán", new InvoiceCheckoutViewModel());
+                    break;
+                case "InvoiceImport":
+                    NavigateTo(page, "Nhập dịch vụ", new InvoiceImportViewModel());
                     break;
                 case "BaoCao":
                 case "Report":
